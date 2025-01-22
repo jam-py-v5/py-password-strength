f6562618bd104469f13bce8d297034dc28fbf33c
Platipusica <babicdd@gmail.com>
2025-01-22 12:47:27 +0800

    v0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='py-password_strength',
-    version='0.0.3-2',
+    version='0.0.4',
     author='Mark Vartanyan',
     author_email='kolypto@gmail.com',
 
